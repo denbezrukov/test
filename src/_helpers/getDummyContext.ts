8dfaceaaf1453861de8e670f29483d98104aaed8
Dominionys <6227442+Dominionys@users.noreply.github.com>
2022-07-24 08:28:21 +0300

    remove inserting canvas into DOM

--- src/_helpers/getDummyContext.ts
+++ src/_helpers/getDummyContext.ts
@@ -4,10 +4,7 @@ export const getDummyContext = () => {
     if (dummyContext) {
         return dummyContext;
     }
-    const fragment = document.createDocumentFragment();
-    const canvas = document.createElement('canvas');
-    fragment.appendChild(canvas);
-    dummyContext = canvas.getContext('2d');
+    dummyContext = document.createElement('canvas').getContext('2d');
 
     return dummyContext;
 };
